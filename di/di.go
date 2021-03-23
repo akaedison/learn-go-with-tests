a160caae53dcc3dee84fa990b36ff67e455317cd
akaedsion <akaedison@icloud.com>
2021-03-23 22:02:03 +0800

    di and http server

---
+++ di/di.go
@@ -0,0 +1,20 @@
+package di
+
+//replace package di to main when you run this
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	_, _ = fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, _ *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+}
